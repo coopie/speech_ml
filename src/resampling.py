bdcc047199205cec6a1ae6d358ed9c0cc81644bd
coopie <sam.j.coope@gmail.com>
2016-05-21 18:50:11 +0100

    Implemented downsampling for toronto corpus

--- src/resampling.py
+++ src/resampling.py
@@ -1,10 +1,20 @@
 # Used for resampling audio to 16KHz
 # For this to work, ffmpeg must be avaliable from the command line
 from subprocess import call
+from keras.utils.generic_utils import Progbar
 
+from sys import argv
+import os
 
 def main():
-    print('not done yet')
+    corpora = argv[1:]
+    num_files = sum([len(os.listdir(x)) for x in corpora])
+    pb = Progbar(num_files)
+    for corpus in corpora:
+        for filename in os.listdir(corpus):
+            if filename.endswith('wav'):
+                downsample(os.path.join(corpus, filename), verbose=False)
+            pb.add(1)
 
 
 def downsample(path, verbose=True):
