5bfe17960f2795f58274b120fbe72de41551d681
Sam Coope <sam.j.coope@gmail.com>
2016-06-17 14:03:04 -0700

    Added TTVExamplesDataSource with tests

--- speech_ml/data_sources.py
+++ speech_ml/data_sources.py
@@ -2,18 +2,22 @@
 """
 
 import os
+from collections import Iterable
 from abc import abstractmethod
+import h5py
+import numpy as np
+
+from .util import yaml_to_dict, ttv_yaml_to_dict
 
 
 class DataSource(object):
     """Base class for all datasources."""
 
     def __getitem__(self, key):
-        """Default interpretation for string based getting."""
-        if type(key) == str:
-            return self._process(key)
+        if isinstance(key, Iterable) and type(key) != str:
+            return (self._process(key) for uri in key)
         else:
-            return [self._process(key) for uri in key]
+            return self._process(key)
 
     @abstractmethod
     def _process(self, ident):
@@ -23,7 +27,7 @@ class DataSource(object):
 class FileDataSource(DataSource):
     """Wrapper around file system"""
 
-    def __init__(self, base_dir, suffix='.wav'):
+    def __init__(self, base_dir='', suffix=''):
         self.base_dir = base_dir
         self.suffix = suffix
 
@@ -49,7 +53,6 @@ class WaveformDataSource(DataSource):
         return self.process_waveform(self.file_source[ident])
 
 
-
 class SpectrogramDataSource(DataSource):
     """Datasource which produces spectrograms from a path to file.
 
@@ -61,6 +64,100 @@ class SpectrogramDataSource(DataSource):
         self.process_spectrogram = process_spectrogram
 
 
-    # TODO: caching
     def _process(self, key):
         return self.process_spectrogram(self.waveform_source[key])
+
+
+
+# TODO: this with lookup_table implemented
+class ExamplesDataSource(DataSource):
+    """Base class for generating training examoples from processed data and metadata"""
+    def __init__(self, data_source, make_target):
+        self.data_source = data_source
+        self.make_target = make_target
+
+
+
+class TTVExamplesDataSource(ExamplesDataSource):
+    """Uses ttv and subject information to build a lookuptable"""
+    def __init__(self, data_source, make_target, ttv, subject_info_dir):
+        super(TTVExamplesDataSource, self).__init__(data_source, make_target)
+        self.ttv = ttv
+        self.subject_info_data_source = FileDataSource(subject_info_dir, suffix='.yaml')
+
+        all_users = {**ttv['test'], **ttv['train']}
+        all_users = {**all_users, **ttv['validation']}
+
+
+        # invert the map
+        uri_to_subjectID = {}
+        for userID in all_users:
+            resources = all_users[userID]
+            for resourceID in resources:
+                uri_to_subjectID[resourceID] = userID
+
+        self.uri_to_subjectID = uri_to_subjectID
+
+
+    def _process(self, key):
+        X = self.data_source[key]
+
+        subjectID = self.uri_to_subjectID[key]
+        Y = self.make_target(X, key, subjectID, self.subject_info_data_source)
+        return X,Y
+
+        # def _create_lookup_table(self):
+        #     """Create a table which rerturns (resource_name, subjectID)"""
+        #     lookup_table = []
+        #     for data_set in ['train', 'validate', 'test']:
+        #         subjects = self.ttv[data_set]
+        #         for subjectID in subjects:
+        #             uris = subjects[subjectID]
+        #             for uri in uris:
+        #                 lookup_table.append(subjectID, uri)
+        #
+        #     return np.array(lookup_table)
+        # assert type(key) == slice or type(key) == int or isinstance(key, Iterable)
+        # if type(key) == slice or isinstance(key, Iterable):
+        #
+        #     lookup_data = lookup_table[key]
+        #     subjectIDs (x[0] for x in lookup_data)
+        #     uris = (x[1] for x in lookup_data)
+        #
+        #     X = self.data_source[uris]
+        #     if type(key) == slice:
+        #         s = key
+        #         Y = (self.make_target(X[i], subjectID, self.subject_info_dir) for i, y in eumerate(range(s.start, s.stop, s.step)))
+        #     elif isinstance(key, Iterable):
+        #         Y = (self.make_target(X[i], subjectID, self.
+        #         ) for i, y in enumerate(key))
+        #
+        #     return (X, Y)
+        # else:
+        #     X = self.data_source[uri]
+        #     Y = self.make_target
+        #
+        # return self.data_source[]
+
+
+
+# TODO
+class CacheNumericalDataSource(DataSource):
+    def __init__(self, cache_name):
+        self.cache = h5py.File(cache_name, 'a')
+        # self.existence_cache = TODO
+
+    def __getitem__(self, key):
+        if type(key) == slice:
+            s = key
+            indices = np.arange(s.start, s.stop, s.step)
+            indices_in_cache = np.fromiter((i, self._in_cache(i)) for i in range(s.start, s.stop, s.step))
+            if len(indices) == indices_in_cache:
+                return self.cache[s]
+            else:
+                return np.array([])
+            # if all in cache, then use slice, else don't
+        elif isinstance(key, Iterable) and type(key) != str:
+            pass
+        else:
+            pass
--- test/data_sources_test.py
+++ test/data_sources_test.py
@@ -4,7 +4,7 @@ import numpy as np
 import numpy.testing as npt
 
 from speech_ml.data_sources import *
-
+from speech_ml.util import yaml_to_dict
 
 DUMMY_DATA_PATH = os.path.join('test', 'dummy_data')
 
@@ -13,7 +13,7 @@ class DataSourcesTests(unittest.TestCase):
 
 
     def test_file_data_source(self):
-        ds = FileDataSource(DUMMY_DATA_PATH)
+        ds = FileDataSource(DUMMY_DATA_PATH, suffix='.wav')
 
         self.assertTrue(
             os.path.isfile(ds['1_sad_kid_1'])
@@ -34,7 +34,7 @@ class DataSourcesTests(unittest.TestCase):
 
 
     def test_waveform_data_source(self):
-        ds = WaveformDataSource(FileDataSource(DUMMY_DATA_PATH), process_waveform=dummy_process_waveforms)
+        ds = WaveformDataSource(FileDataSource(DUMMY_DATA_PATH, suffix='.wav'), process_waveform=dummy_process_waveforms)
 
         self.assertTrue(
             np.all(
@@ -54,12 +54,10 @@ class DataSourcesTests(unittest.TestCase):
         ds = \
             SpectrogramDataSource(
                 WaveformDataSource(
-                    FileDataSource(DUMMY_DATA_PATH),
+                    FileDataSource(DUMMY_DATA_PATH, suffix='.wav'),
                     process_waveform=dummy_process_waveforms),
                 dummy_process_spectrograms
             )
-
-        print(ds['1_sad_kid_1'])
 
         self.assertTrue(
             np.all(
@@ -78,6 +76,34 @@ class DataSourcesTests(unittest.TestCase):
 
 
 
+    def test_ttv_examples_generator(self):
+        data_source = DummyDataSource()
+
+        def make_target(X, key, subjectID, subject_info_data_source):
+            metadata = yaml_to_dict(subject_info_data_source[subjectID])
+            return metadata['legs']
+
+
+        subject_info_dir = os.path.join('test', 'dummy_data', 'metadata')
+        ttv = yaml_to_dict(os.path.join(subject_info_dir, 'dummy_ttv.yaml'))
+
+        examples_ds = TTVExamplesDataSource(data_source, make_target, ttv, subject_info_dir)
+
+        self.assertTrue(
+            examples_ds['blorp_2'],
+            (data_source.data['blorp_2'], 1)
+        )
+        self.assertTrue(
+            examples_ds['blerp_1'],
+            (data_source.data['blerp_1'], 2)
+        )
+        self.assertTrue(
+            examples_ds['shlerp_322'],
+            (data_source.data['shlerp_322'], 3)
+        )
+
+
+
 def dummy_process_waveforms(path):
     """A way of identifying which file the dummy waveform comes from."""
     filename_split = path.split(os.sep)[-1].split('.')[0].split('_')
@@ -88,6 +114,21 @@ def dummy_process_waveforms(path):
 
 def dummy_process_spectrograms(waveform):
     return np.eye(2) * waveform
+
+
+class DummyDataSource(DataSource):
+    def __init__(self):
+        self.data = {
+            'blorp_2': np.eye(2) * 1,
+            'blerp_1': np.eye(2) * 2,
+            'shlerp_322': np.eye(2) * 3
+        }
+
+    def _process(self, key):
+        return self.data[key]
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
